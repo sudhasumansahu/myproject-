706d162aa2a7f77a0c4b216589180db6cdd81f35
sudhasumansahu <40867928+sudhasumansahu@users.noreply.github.com>
2020-02-22 01:10:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+/*to open and close chat window*/
+function openForm(){
+	document.getElementById("myForm").style.display="block";
+}
+
+function closeForm(){
+	document.getElementById("myForm").style.display="none";
+}
+
